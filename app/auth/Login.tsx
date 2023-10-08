f2b3090a4df1bac5682d85c74fcd33be3c015308
Charilaou Konstantinos <charilaou.c@gmail.com>
2023-10-08 05:42:22 +0300

    nextauth and database configured

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -6,7 +6,7 @@ export default function Login(){
     return(
         <li className="list-none">
             <button  
-            onClick={() => signIn} 
+            onClick={() => signIn('google')} 
             className="text-sm bg-gray-700 text-white py-2 px-6 rounded-xl disabled:opacity-25">
                 Sign In
             </button>
